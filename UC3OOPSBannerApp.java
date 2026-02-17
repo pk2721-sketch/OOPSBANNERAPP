0f13972458299150afb000501fd47ba0244f1c5b
Partha Pratim Kalita <pk2721@srmist.edu.in>
2026-02-17 10:20:47 +0530

    Add files via upload

---
+++ UC3OOPSBannerApp.java
@@ -0,0 +1,15 @@
+public class UC3OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println(String.join("","    ***   " + " " + "    ***   " + " " + "***** " + " " + "  ***** "));
+		System.out.println(String.join(""," **    ** " + " " + " **    ** " + " " + "*   **" + " " + " **     "));
+		System.out.println(String.join("","**      **" + " " + "**      **" + " " + "*   **" + " " + "**      "));
+		System.out.println(String.join("","**      **" + " " + "**      **" + " " + "*  ** " + " " + " *****  "));
+		System.out.println(String.join("","**      **" + " " + "**      **" + " " + "***   " + " " + "     ** "));
+		System.out.println(String.join(""," **    ** " + " " + " **    ** " + " " + "*     " + " " + " **  ** "));
+		System.out.println(String.join("","   ***    " + " " + "   ***    " + " " + "*     " + " " + "  ***** "));
+	
+		}
+	
+	}
+    
+
